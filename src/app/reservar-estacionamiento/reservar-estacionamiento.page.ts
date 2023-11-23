8f3cdb3d8c85ab878d750cb3f0988761b7e12968
gera-cl <hernandezf.german@gmail.com>
2023-11-23 02:02:31 -0300

    reservar estacionamiento

--- src/app/reservar-estacionamiento/reservar-estacionamiento.page.ts
+++ src/app/reservar-estacionamiento/reservar-estacionamiento.page.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservar-estacionamiento.page.scss'],
 })
 export class ReservarEstacionamientoPage implements OnInit {
+  estacionamientos: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    fetch("http://localhost:3000/estacionamiento?estado=disponible", { method: 'GET' })
+      .then(async response => {
+        const estacionamientos = await response.json();
+        this.estacionamientos = estacionamientos;  
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  reservar(id: String) {
+    fetch(`http://localhost:3000/estacionamiento/${id}?estado=reservado`, { method: 'PUT' })
+    .then(async response => {
+      const estacionamiento = await response.json();
+      this.estacionamientos = this.estacionamientos.filter(e => e._id !== estacionamiento._id)
+    })
+    .catch(error => console.log('error', error));
   }
 
 }
