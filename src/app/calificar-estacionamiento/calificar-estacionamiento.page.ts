9de091b0f377d0119443b83b07fdc50feba74f18
Hernandez <joaquin.hernandez@culligan.cl>
2023-12-05 12:08:06 -0300

    intento de endpoint reserva/historial/calificacion

--- src/app/calificar-estacionamiento/calificar-estacionamiento.page.ts
+++ src/app/calificar-estacionamiento/calificar-estacionamiento.page.ts
@@ -9,18 +9,31 @@ import { NavController } from '@ionic/angular';
 })
 export class CalificarEstacionamientoPage implements OnInit {
 
+  historials: any
   estacionamiento: any
+  puntuacion:number=0
+  comentario: string = ""
+
   constructor(public route: Router, public nav: NavController) {
     const estacionamientoString: string | null = localStorage.getItem("calificar-estacionamiento")
     if (estacionamientoString !== null) {
       const estacionamientoObject = JSON.parse(estacionamientoString)
       this.estacionamiento = estacionamientoObject
     }
+
+    const his: string | null=localStorage.getItem("his")
+    if (his!==null){
+    const historials = JSON.parse(his)
+    this.historials=historials
+    }
   }
 
   ngOnInit() {
   }
 
+  submitQuality(){
+    console.log("El submit funciona");
+  }
   volverCliente() {
     this.nav.navigateForward("cliente")
   }
